300a780f62abb4d043c099d9883d3d0046389af8
jannat8 <bsef14a551@pucit.edu.pk>
2016-06-01 16:44:53 +0000

    Update myclientM2.py

--- myclientM2.py
+++ myclientM2.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python
 import socket               # Import socket module
+import sys
 import threading
 from thread import *
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
 host = socket.gethostname()
 port = 5188    
 s.connect((host, port))
-i=0
+
 def receive():
-    #data=string.split[' ']
+    #data=string.split(' ']
     
     while True :
       #print "Typing......."
       reply=s.recv(1024)
       if not reply:
               break
-      print repr(reply)
+      print "\nRecieved: ",repr(reply)
       
     #print s.recv(1024)
 
 
 
 start_new_thread(receive,())
+
 while True :
   data=raw_input("Enter Message: ")
   s.send(data)
+
+
 s.close()                     # Close the socket when done 
